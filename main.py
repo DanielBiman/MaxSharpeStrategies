c68574bf234c3f94374a691da0d169d3673de3b1
Daniel Biman <daniel.b@tutamail.com>
2026-07-23 23:05:32 +0200

    add missing comma in sell order qty calculation
    qty = 0.1
              ^^^
    SyntaxError: invalid syntax. Perhaps you forgot a comma?

--- main.py
+++ main.py
@@ -81,9 +81,9 @@ elif action == 2:
             symbol=position.symbol,
             # qty=position.qty,
             # Reduced qty due to 200k order size limit from Alpaca
-            # qty = [200000/float(position.current_price) if float(position.qty)*float(position.current_price) > 200000 else float(position.qty)] # SyntaxError: invalid syntax. Perhaps you forgot a comma?
+            qty = [200000/float(position.current_price) if float(position.qty)*float(position.current_price) > 200000 else float(position.qty)],
             # For testing:
-            qty = 0.1
+            # qty = 0.1
 
             side=OrderSide.SELL,
             time_in_force=TimeInForce.GTC
